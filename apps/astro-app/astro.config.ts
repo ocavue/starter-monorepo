7c6523bf611e4371f48b849dbf96635b68d42655
ocavue <ocavue@gmail.com>
2025-04-30 02:22:47 +1000

    add adapter

--- apps/astro-app/astro.config.ts
+++ apps/astro-app/astro.config.ts
@@ -1,10 +1,12 @@
 import react from '@astrojs/react'
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 const config: unknown = defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
