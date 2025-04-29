69d56752a767348ea55eeda9f6a340e502bca797
ocavue <ocavue@gmail.com>
2025-04-29 22:54:15 +1000

    update config

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,17 +1,18 @@
-// @ts-check
 import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
+import nova from 'starlight-theme-nova'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: 'My Docs',
+      plugins: [nova()],
       social: [
         {
           icon: 'github',
           label: 'GitHub',
-          href: 'https://github.com/withastro/starlight',
+          href: 'https://github.com/ocavue/starter-monorepo',
         },
       ],
       sidebar: [
