c12981cd6cd85b7de2e0c5d155227a0f63f17e98
ocavue <ocavue@users.noreply.github.com>
2025-05-31 20:33:02 +1000

    feat: update eslint rules (#11)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-import { basic, markdown, react } from '@ocavue/eslint-config'
+import { defineESLintConfig } from '@ocavue/eslint-config'
 
-/** @type {import('eslint').Linter.Config[]} */
-const configs = [...basic(), ...markdown(), ...react()]
-
-export default configs
+export default defineESLintConfig({ react: true })
