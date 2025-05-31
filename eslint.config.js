860339f0faa7cdf0c83b6a736a98ff21caaf97d9
ocavue <ocavue@gmail.com>
2025-05-31 20:29:27 +1000

    update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
-import { defineConfig } from '@ocavue/eslint-config'
+import { defineESLintConfig } from '@ocavue/eslint-config'
 
-export default defineConfig({ react: true })
+export default defineESLintConfig({ react: true })
