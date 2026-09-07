b568e9077f748b981be5465741689b42f083fda2
ocavue <ocavue@users.noreply.github.com>
2026-09-07 12:13:53 +1000

    chore(deps): update dependencies (#52)

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
         },
         {
           label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          items: [{ autogenerate: { directory: 'reference' } }],
         },
       ],
     }),
